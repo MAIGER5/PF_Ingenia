bd7fd5adfa8789508bc12b1f8e090a389706328e
Davidc480 <Davidcalle.480@gmail.com>
2023-05-10 11:05:54 -0500

    refactor (api/src/harcodeo/user - cursos - datos - instructor) functional data to consume

--- api/src/harcodeo (borrar al final)/data.js
+++ api/src/harcodeo (borrar al final)/data.js
@@ -5,7 +5,7 @@ const data = [
     lastname: "Pérez",
     correo: "juan.perez@gmail.com",
     password: "4d10e11d9b8c98c7",
-    imgProfile: "https://example.com/profile-pic.jpg",
+    imgProfile: "https://acortar.link/IYsS7C",
   },
   {
     idData: 2,
@@ -13,7 +13,8 @@ const data = [
     lastname: "González",
     correo: "maria.gonzalez@gmail.com",
     password: "a320f29eae139",
-    imgProfile: "https://example.com/profile-pic-2.jpg",
+    imgProfile:
+      "https://acontecendoaqui.com.br/wp-content/uploads/2022/01/nfttwitter_destaque.jpg",
   },
   {
     idData: 3,
@@ -21,7 +22,7 @@ const data = [
     lastname: "Ramírez",
     correo: "luis.ramirez@gmail.com",
     password: "30a31bcfae7",
-    imgProfile: "https://example.com/profile-pic-3.jpg",
+    imgProfile: "https://pbs.twimg.com/media/FJ_qyEIXIAIjnc7.jpg",
   },
   {
     idData: 4,
@@ -29,7 +30,8 @@ const data = [
     lastname: "Rodríguez",
     correo: "ana.rodriguez@gmail.com",
     password: "d9d1606bdf8f107",
-    imgProfile: "https://example.com/profile-pic-4.jpg",
+    imgProfile:
+      "https://cdn.autobild.es/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2016/09/569465-whatsapp-que-tus-contactos-ponen-rana-perfil.jpg?itok=tpvHWpeZ",
   },
   {
     idData: 5,
@@ -37,7 +39,8 @@ const data = [
     lastname: "Hernández",
     correo: "pedro.hernandez@gmail.com",
     password: "f296f48a77",
-    imgProfile: "https://example.com/profile-pic-5.jpg",
+    imgProfile:
+      "https://us.123rf.com/450wm/fizkes/fizkes2104/fizkes210400405/167446118-cierra-el-retrato-de-la-pantalla-de-la-cabeza-de-un-joven-cauc%C3%A1sico-sonriente-que-mira-la.jpg?ver=6",
   },
   {
     idData: 6,
@@ -45,7 +48,8 @@ const data = [
     lastname: "López",
     correo: "sofia.lopez@gmail.com",
     password: "a1654e4e6d",
-    imgProfile: "https://example.com/profile-pic-6.jpg",
+    imgProfile:
+      "https://i.pinimg.com/736x/b5/49/41/b5494197b2d462c940f88988b203d290.jpg",
   },
   {
     idData: 7,
@@ -53,7 +57,8 @@ const data = [
     lastname: "Martínez",
     correo: "jorge.martinez@gmail.com",
     password: "ceeb163e74d",
-    imgProfile: "https://example.com/profile-pic-7.jpg",
+    imgProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYPcQLLlnqnepooI2vNWbKKH6lo0bFMeH3aLieN-mlOI5cAwDyeCyqh2jYA1gs94URzGc&usqp=CAU",
   },
   {
     idData: 8,
@@ -61,7 +66,8 @@ const data = [
     lastname: "García",
     correo: "lucia.garcia@gmail.com",
     password: "78a4067a1e",
-    imgProfile: "https://example.com/profile-pic-8.jpg",
+    imgProfile:
+      "https://i.pinimg.com/236x/16/7e/ce/167ece7833c7924e019064367fe0393b.jpg",
   },
   {
     idData: 9,
@@ -69,7 +75,8 @@ const data = [
     lastname: "Sanchez",
     correo: "raul.sanchez@gmail.com",
     password: "92d39d1bc10765a97db0",
-    imgProfile: "https://example.com/profile-pic-9.jpg",
+    imgProfile:
+      "https://e0.pxfuel.com/wallpapers/442/989/desktop-wallpaper-perfil-boy-face-thumbnail.jpg",
   },
   {
     idData: 10,
@@ -77,7 +84,97 @@ const data = [
     lastname: "Pérez",
     correo: "carla.perez@gmail.com",
     password: "4d9f29b8a7d20ed19b3",
-    imgProfile: "https://example.com/profile-pic-10.jpg",
+    imgProfile:
+      "https://media.tycsports.com/files/2022/06/14/440409/las-20-mejores-fotos-de-perfil-para-tu-cuenta-de-free-fire_w416.webp",
+  },
+  {
+    idData: 11,
+    name: "Juan",
+    lastname: "García",
+    correo: "juan.garcia@gmail.com",
+    password: "1a2b3c4d5e6f7g8h9i",
+    imgProfile:
+      "https://www.okchicas.com/wp-content/uploads/2018/01/Poses-para-una-buena-foto-de-perfil-1-1.jpg",
+  },
+  {
+    idData: 12,
+    name: "Laura",
+    lastname: "González",
+    correo: "laura.gonzalez@gmail.com",
+    password: "a1b2c3d4e5f6g7h8i9",
+    imgProfile: "https://definicion.de/wp-content/uploads/2010/07/perfil-1.jpg",
+  },
+  {
+    idData: 13,
+    name: "Pedro",
+    lastname: "Sánchez",
+    correo: "pedro.sanchez@gmail.com",
+    password: "1234567890qwertyuiop",
+    imgProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-AzwV9lTyXUV-FrOVY9XX2orGXu7_HeJoSEydr4ah1JCDS5vD-QxV0xBm0_xWvRFisVA&usqp=CAU",
+  },
+  {
+    idData: 14,
+    name: "María",
+    lastname: "Rodríguez",
+    correo: "maria.rodriguez@gmail.com",
+    password: "qwertyuiop1234567890",
+    imgProfile:
+      "https://st.depositphotos.com/67617362/59367/i/600/depositphotos_593678724-stock-photo-young-man-using-a-chat.jpg",
+  },
+  {
+    idData: 15,
+    name: "Ricardo",
+    lastname: "Gómez",
+    correo: "ricardo.gomez@gmail.com",
+    password: "p@ssw0rd",
+    imgProfile:
+      "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    idData: 16,
+    name: "Isabel",
+    lastname: "López",
+    correo: "isabel.lopez@gmail.com",
+    password: "password123",
+    imgProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS09EOrGn6PLD1TE8qpYNf5ItBRTX9mK3jpw&usqp=CAU",
+  },
+  {
+    idData: 17,
+    name: "Miguel",
+    lastname: "Fernández",
+    correo: "miguel.fernandez@gmail.com",
+    password: "m1guelFern",
+    imgProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpkzH9jE2zeQaJCxONDTcJv6mYXN7-z2sWSg&usqp=CAU",
+  },
+  {
+    idData: 18,
+    name: "Ana",
+    lastname: "Jiménez",
+    correo: "ana.jimenez@gmail.com",
+    password: "J1m3n3z",
+    imgProfile:
+      "https://vivolabs.es/wp-content/uploads/2022/03/perfil-mujer-vivo.png",
+  },
+  {
+    idData: 19,
+    name: "Pablo",
+    lastname: "Torres",
+    correo: "pablo.torres@gmail.com",
+    password: "pabl0t0rr3s",
+    imgProfile:
+      "https://www.lavanguardia.com/files/og_thumbnail/uploads/2018/09/27/5fa44cf4543ff.jpeg",
+  },
+  {
+    idData: 20,
+    name: "Elena",
+    lastname: "Ruiz",
+    correo: "elena.ruiz@gmail.com",
+    password: "e1en1t4ru1z",
+    imgProfile:
+      "https://us.123rf.com/450wm/havucvp/havucvp2011/havucvp201100093/158545557-retrato-de-una-mujer-asi%C3%A1tica-sosteniendo-su-cabeza-debido-a-un-dolor-de-cabeza.jpg?ver=6",
   },
 ];
 
--- api/src/harcodeo (borrar al final)/instructor.js
+++ api/src/harcodeo (borrar al final)/instructor.js
@@ -1,39 +1,82 @@
-const instructor = [
+[
+  {
+    idInstructor: 1,
+    description:
+      "Experienced yoga instructor with over 10 years of practice in various styles including Hatha and Vinyasa. Passionate about helping students find their inner peace and strength through yoga.",
+    studies:
+      "200-hour RYT certification from Yoga Alliance, advanced training in Hatha and Vinyasa yoga.",
+    asset: true,
+  },
   {
     idInstructor: 2,
-    description: "Instructor de desarrollo web",
-    studies: "Licenciado en Informática",
+    description:
+      "Certified personal trainer with 5 years of experience helping clients achieve their fitness goals through customized workout plans and nutritional guidance. Specializes in strength training and weight loss.",
+    studies:
+      "Bachelor's degree in Exercise Science, ACE certified personal trainer.",
     asset: false,
-    imgUrl: "https://picsum.photos/id/237/200/300",
   },
   {
     idInstructor: 3,
-    description: "Instructor de diseño gráfico",
-    studies: "Diseñador Gráfico Profesional",
+    description:
+      "Experienced dance instructor specializing in contemporary and ballet styles. Trained at prestigious dance academies in Europe and Asia, with 8 years of teaching experience.",
+    studies:
+      "Bachelor's degree in Dance from London Contemporary Dance School, additional training in ballet from Royal Ballet School and contemporary dance from Hong Kong Academy for Performing Arts.",
     asset: true,
-    imgUrl: "https://picsum.photos/id/238/200/300",
   },
   {
     idInstructor: 4,
-    description: "Instructor de marketing digital",
-    studies: "Licenciado en Marketing",
-    asset: false,
-    imgUrl: "https://picsum.photos/id/239/200/300",
+    description:
+      "Passionate chef with 15 years of experience in various cuisines including French, Italian, and Japanese. Specializes in creating unique and flavorful dishes using locally sourced and sustainable ingredients.",
+    studies:
+      "Culinary arts degree from Le Cordon Bleu, additional training in Japanese cuisine from Tsuji Culinary Institute.",
+    asset: true,
   },
   {
     idInstructor: 5,
-    description: "Instructor de bases de datos",
-    studies: "Ingeniero en Sistemas",
-    asset: true,
-    imgUrl: "https://picsum.photos/id/240/200/300",
+    description:
+      "Experienced language instructor with 7 years of teaching experience in Spanish and French. Focuses on practical conversation skills and cultural immersion.",
+    studies:
+      "Bachelor's degree in Spanish and French from University of Madrid, certified language instructor.",
+    asset: false,
   },
   {
     idInstructor: 6,
-    description: "Instructor de programación en Java",
-    studies: "Licenciado en Informática",
+    description:
+      "Experienced music instructor specializing in guitar and piano. Trained at prestigious music schools in the US and Europe, with 10 years of teaching experience.",
+    studies:
+      "Bachelor's degree in Music from Berklee College of Music, additional training in classical guitar from Royal Conservatory of Music.",
     asset: true,
-    imgUrl: "https://picsum.photos/id/241/200/300",
+  },
+  {
+    idInstructor: 7,
+    description:
+      "Experienced art instructor specializing in painting and drawing. Trained at prestigious art schools in Europe and the US, with 8 years of teaching experience.",
+    studies:
+      "Master's degree in Fine Arts from School of the Art Institute of Chicago, additional training in painting and drawing from Florence Academy of Art.",
+    asset: false,
+  },
+  {
+    idInstructor: 8,
+    description:
+      "Experienced software engineer with 12 years of experience in developing web applications using JavaScript and Python. Passionate about teaching programming to beginners and helping them become confident developers.",
+    studies:
+      "Bachelor's degree in Computer Science from Stanford University, advanced training in full-stack web development.",
+    asset: true,
+  },
+  {
+    idInstructor: 9,
+    description:
+      "Experienced marketing professional with 8 years of experience in developing and executing marketing campaigns for Fortune 500 companies. Specializes in digital marketing and brand strategy.",
+    studies:
+      "Bachelor's degree in Marketing from University of California, Los Angeles, MBA from Harvard Business School.",
+    asset: false,
+  },
+  {
+    idInstructor: 10,
+    description:
+      "Experienced photographer with 10 years of experience in fashion, portrait, and commercial photography. Passionate about capturing the beauty of the world through the lens of a camera.",
+    studies:
+      "Bachelor's degree in Photography from New York University, additional training in fashion photography from International Center",
+    asset: false,
   },
 ];
-
-module.exports = instructor;
