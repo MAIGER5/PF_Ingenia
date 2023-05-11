e033bdc7f4a03f643677ba3f278a02db0189d78f
Davidc480 <Davidcalle.480@gmail.com>
2023-05-11 01:22:44 -0500

    refactor(api/src/models/Data) add bcrypt library

--- api/src/models/Data.js
+++ api/src/models/Data.js
@@ -1,38 +1,57 @@
-const { DataTypes } = require('sequelize');
+//Hay que instalar esta libreria.
+// const bcrypt = require('bcrypt');
+
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Data", {
-    idData: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastname:{
+  sequelize.define(
+    "Data",
+    {
+      idData: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    correo:{
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      correo: {
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
-    },
-    password:{ 
-        type:DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
         validate: {
-            is: /^[0-9a-f]{64}$/i
-          },
-        allowNull:false,
-    },
-    imgProfile:{
-        type:DataTypes.STRING,
+          is: /^[0-9a-f]{64}$/i,
+        },
+        allowNull: false,
+      },
+
+      //Esta es una librería que se encarga de validar las contraseñas, no afectara el modelo ya que no se sube a la base de datos
+      // },
+      // {
+      //   sequelize,
+      //   modelName: "Data",
+      //   hooks: {
+      //     beforeCreate: (data) => {
+      //       const salt = bcrypt.genSaltSync();
+      //       data.password = bcrypt.hashSync(data.password, salt);
+      //     },
+      //   },
+      imgProfile: {
+        type: DataTypes.STRING,
         isUrl: true,
         allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
     }
-  },{
-    timestamps:false,
-});
+  );
 };
