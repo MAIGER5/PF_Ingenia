84300340e4ba8f174b0fcd8146ccd5876d9ac0b1
Davidc480 <Davidcalle.480@gmail.com>
2023-05-11 18:01:11 -0500

    refactor(src/routes/index.routes.js) modified route

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const courseRouter = require("./course.routes");
 const userRoutes = require("./user.routes");
-const instructorRouter = require('./instructor.routes')
+const instructorRouter = require("./instructor.routes");
 
 const router = Router();
 
-router.use("/instructor",instructorRouter)
+router.use("/instructor", instructorRouter);
 
 router.use("/courses", courseRouter);
 
