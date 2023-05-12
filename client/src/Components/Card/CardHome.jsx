b33cadffeaaaa11648436191f28aaa2ad75db885
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-05-11 23:49:06 -0500

    feat(Renderizado del harcodeo del detalle del curso, creacion del store, reducer, actions, actualizacion ruta detalle del curso)

--- client/src/Components/Card/CardHome.jsx
+++ client/src/Components/Card/CardHome.jsx
@@ -6,54 +6,60 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Box, Button, CardActionArea, Rating } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 
-function CardHome({title,imgCurso,instructor,price,ratings}) {
+function CardHome({title,imgCurso,instructor,price,ratings, id}) {
  
  
   return (
-    <Card sx={{ 
-      transition: '0.2s', 
-      '&:hover': {
-        transform: 'scale(1.05)'
-      }, 
-      maxWidth: 380,
-      height:450
-    }}
-    >
-      <CardActionArea>
-        <CardMedia
-          sx={{ height: 190 }}
-          component='img'
 
-          image={imgCurso}
-          title="green iguana"
+    <Link to={`/DetailCourse/${id}`}>
+      <Card sx={{ 
+        transition: '0.2s', 
+        '&:hover': {
+          transform: 'scale(1.05)'
+        }, 
+        maxWidth: 380,
+        height:450
+      }}
+      >
+        {/* <Link to={`/CoureseDetail/${id}`} > */}
+          <CardActionArea >
+            <CardMedia
+              sx={{ height: 190 }}
+              component='img'
 
-        />
+              image={imgCurso}
+              title="xxxxx"
 
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
-          {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           {instructor}
-          </Typography>
-        </CardContent>
+            />
 
-        <Rating name="read-only" value={ratings} readOnly sx={{ color: "#e91e63" }} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+              {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              {instructor}
+              </Typography>
+            </CardContent>
+
+            <Rating name="read-only" value={ratings} readOnly sx={{ color: "#e91e63" }} />
 
 
-        <CardActions>
-          <Button variant='contained' startIcon={<ShoppingCartIcon/>} 
-          >Add
-          </Button>
-        </CardActions>
-        <Box component='h3'>
-         ${price}
-        </Box>
+            <CardActions>
+              <Button variant='contained' startIcon={<ShoppingCartIcon/>} 
+              >Add
+              </Button>
+            </CardActions>
+            <Box component='h3'>
+            ${price}
+            </Box>
 
-      </CardActionArea>
-    </Card>
+          </CardActionArea>
+        {/* </Link> */}
+      </Card>
+    </Link>
   );
 };
 
---
+++ client/src/Redux/Actions/cleanDetail.js
@@ -0,0 +1,7 @@
+export const CLEAN_DETAIL = 'CLEAN_DETAIL';
+
+export const cleandDetail = ()=> {
+    return {
+        type: CLEAN_DETAIL
+    };
+};
