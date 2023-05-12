a215d8a379a15e1e85646ce74e238bb6fc5939ac
Davidc480 <Davidcalle.480@gmail.com>
2023-05-11 22:50:58 -0500

    refactory(src/controllers/userControlers) rename files

--- api/src/controllers/userControllers/userPostController.js
+++ api/src/controllers/userControllers/userPostController.js
@@ -1,7 +1,6 @@
 const { User, Course } = require("../../database");
 
 const postUser = async (name, lastname, password, email) => {
-  console.log(`4, ${(name, lastname, password, email)}`);
   //Definimos su roll
   const Is = "STUDEN";
   //crea un nuevo usuario en la base de datos
@@ -18,12 +17,3 @@ const postUser = async (name, lastname, password, email) => {
 };
 
 module.exports = postUser;
-
-// Datos de ejemplo para crear un usuario.
-
-// {
-//   "name": "Juan",
-//   "lastName": "Pérez",
-//   "password": "david08",
-//   "email": "jasddn@gmail.com"
-// }
