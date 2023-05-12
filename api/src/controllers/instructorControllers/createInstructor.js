9ea6db110d22df528d6231cec7622abcdd9e6f68
Davidc480 <Davidcalle.480@gmail.com>
2023-05-12 14:52:32 -0500

    refactor(src/controllers/userControllers/userAuthenticatedController.js) authentication password and roles

--- api/src/controllers/instructorControllers/createInstructor.js
+++ api/src/controllers/instructorControllers/createInstructor.js
@@ -1,17 +1,18 @@
-const {User,Course} = require('../../database')
+const { User, Course } = require("../../database");
+const userDataForm = require("../../helper/userDataForm");
 
-const createInstructor = async (name,lastname,email,password,studies)=>{
-    console.log(name)
-    const Is ='INSTRUCTOR';
-    const newUser = await User.create({
-        name,
-        lastname,
-        email,
-        password,
-        studies,
-        Is,
-        asset:true
-    })
-    return newUser;
-}
+const createInstructor = async (name, lastname, email, password, studies) => {
+  console.log(name);
+  const Is = "INSTRUCTOR";
+  const newUser = await User.create({
+    name,
+    lastname,
+    email,
+    password,
+    studies,
+    Is,
+    asset: true,
+  });
+  return { user: userDataForm(newUser) };
+};
 module.exports = createInstructor;
