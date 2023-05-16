45bfed9cebd9b110b05223f4ec4f7a780c76571f
new_proyect_01 <mafilz145@gmail.com>
2023-05-16 18:28:53 -0400

    Rutas de categoria

--- api/src/handlers/category/category.handlers.js
+++ api/src/handlers/category/category.handlers.js
@@ -1,5 +1,17 @@
 const getCategoryControllers = require('../../controllers/categoryControllers/getCategoryControllers')
 const postCategoryControllers = require('../../controllers/categoryControllers/postCategoryControllers')
+const getCategoryByIdControllers = require('../../controllers/categoryControllers/getCategoryByIdControllers')
+
+const categoryIdHandlers = async (req,res)=>{
+    const { id } = req.params;
+    try {
+        const response = await getCategoryByIdControllers(id)
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 const categoryHandlers = async (req,res)=>{
     try {
         const response = await getCategoryControllers();
@@ -19,5 +31,6 @@ const posCategoryControllers = async (req,res)=>{
 }
 module.exports={
     categoryHandlers,
-    posCategoryControllers
+    posCategoryControllers,
+    categoryIdHandlers
 }
