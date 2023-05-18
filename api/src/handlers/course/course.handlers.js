b0e84c23a7b35284466bd51581600edb49c65afc
fellgar246 <fellgar246@gmail.com>
2023-05-17 20:03:04 -0600

    refactor(ajuste ruta POST /courses)

--- api/src/handlers/course/course.handlers.js
+++ api/src/handlers/course/course.handlers.js
@@ -42,52 +42,54 @@ const createCourseHandlers = async (req, res) => {
     users,
     catego} = req.body;
   try {
-    
-    if(image){
+    // console.log("image", title);
+    // if(image){
+    //   console.log("dentro del if", image)
+    //   const uploadImage = await cloudinary.uploader.upload(image, {
+    //     upload_preset: 'ingenia',
+    //   });
 
-      const uploadImage = await cloudinary.uploader.upload(image, {
-        upload_preset: 'ingenia',
-      });
+    //   if(uploadImage){
+    //     const response = await createCourse(
+    //       title,
+    //       description,
+    //       uploadImage.url,
+    //       lenguage,
+    //       price,
+    //       pro,
+    //       duration,
+    //       content,
+    //       dificulty,
+    //       requirement,
+    //       learnTo,
+    //       studyMethod,
+    //       users,
+    //       catego
+    //     );
+    //     res.status(200).json(response);
+    //   }
 
-      if(uploadImage){
-        const response = await createCourse(
-          title,
-          description,
-          uploadImage.url,
-          lenguage,
-          price,
-          pro,
-          duration,
-          content,
-          dificulty,
-          requirement,
-          learnTo,
-          studyMethod,
-          users,
-          catego
-        );
-        res.status(200).json(response);
-      }
-
-    }
-
-    // const response = await createCourse(
-    //   title,
-    //   description,
-    //   image,
-    //   lenguage,
-    //   price,
-    //   pro,
-    //   duration,
-    //   content,
-    //   dificulty,
-    //   requirement,
-    //   learnTo,
-    //   studyMethod,
-    //   users,
-    //   catego
-    // );
-    // res.status(200).json(response);
+    // }
+    // else {
+    //   throw Error("no se logro")
+    // }
+    const response = await createCourse(
+      title,
+      description,
+      image,
+      lenguage,
+      price,
+      pro,
+      duration,
+      content,
+      dificulty,
+      requirement,
+      learnTo,
+      studyMethod,
+      users,
+      catego
+    );
+    res.status(200).json(response);
   } catch (error) {
     console.log(error);
     return res.status(404).json({ msg: "Failed to create course", error });
