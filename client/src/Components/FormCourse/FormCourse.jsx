daed107232a3c4de1e78e5ed60a1204ece193958
fellgar246 <fellgar246@gmail.com>
2023-05-18 17:05:12 -0600

    refactor(api/routes/courses,client/pages/postCourse)

--- client/src/Components/FormCourse/FormCourse.jsx
+++ client/src/Components/FormCourse/FormCourse.jsx
@@ -1,256 +1,243 @@
 import { useState } from "react";
 import axios from "axios";
+import { categoryOptions } from "./validations";
 import styles from "./FormCourse.module.css"
 
-export default function FormCourse() {
+export default function FormCourseCopy() {
 
-
-    const [fileInputState, setFileInputState] = useState('');
-    const [previewSource, setPreviewSource] = useState('');
-    const [selectedFile, setSelectedFile] = useState();
-    const [course, setCourse] = useState({
-        title:"",
-        description:"",
-        image: "",
-        lenguage:"",
-        price:"",
-        pro:false,
-        duration:"", 
-        content:"",
-        dificulty:"",
-        requirement:"",
-        learnTo:[],
-        studyMethod:""
-    })
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [lenguage, setLenguage] = useState("")
+    const [price, setPrice] = useState("")
+    const [pro, setPro] = useState(false)
+    const [duration, setDuration] = useState("")
+    const [content, setContent] = useState("")
+    const [dificulty, setDificulty] = useState("")
+    const [requirement, setRequirement] = useState("")
+    const [learnTo, setLearnTo] = useState([])
+    const [studyMethod, setStudyMethod] = useState("")
+    const [productImg, setProductImg] = useState("");
+    const [category, setCategory] = useState("");
+    const [previewSource, setPreviewSource] = useState("");
 
     const handleSubmit = async(event) => {
         event.preventDefault();
-        let imagen;
-        if (!selectedFile) return;
-        const reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onloadend = () => {
-            //uploadImage(reader.result);
-            // course.image = reader.result
-            imagen = reader.result
-            //console.log("normal", reader.result)
-        };
-        reader.onerror = () => {
-            console.error("There is an error");
-        };
-        course.learnTo = course.learnTo.split(','); 
-        course.price = Number(course.price)
-        course.image = imagen
-        console.log("inicial",course);
-        // await axios.post('http://localhost:3001/courses/', course,{
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     }
-        // })
-        // .then((response) => {
-        //     console.log(response.data)
-        //   })
-        //   .catch((error) => {
-        //     console.log(error.response?.data);
-        //   });
+    
+        console.log({
+            title,
+            description,
+            image: productImg,
+            lenguage,
+            price,
+            pro,
+            duration, 
+            content,
+            dificulty,
+            requirement,
+            learnTo: learnTo.split(','),
+            studyMethod
+          });
 
-        // await axios.post('http://localhost:3001/courses/', JSON.stringify(course))
-        //     .then((response) => {
-        //         console.log(response.data)
-        //     })
-        //     .catch((error) => {
-        //         console.log(error.response?.data);
-        // });
-       
-        // await axios({
-        //     method: 'POST', url: 'http://localhost:3001/courses/'
-        //     , data: { course }
-        //     , headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // .then((response) => {
-        //     console.log(response.data)
-        //     })
-        //     .catch((error) => {
-        //     console.log(error.response?.data);
-        // });
-
-        // try {
-        //     await fetch('http://localhost:3001/courses/', {
-        //         method: 'POST',
-        //         body: JSON.stringify({ data: course }),
-        //         headers: { 'Content-Type': 'application/json' },
-        //     });
-        //     setFileInputState('');
-        //     setPreviewSource('');
-
-        // } catch (err) {
-        //     console.error(err);
-          
-        // }
-
-        setFileInputState('');
-        setPreviewSource('');
-        console.log(course);
-        setCourse({
-            title:"",
-            description:"",
-            image:"",
-            lenguage:"",
-            price:"",
-            pro:false,
-            duration:"", 
-            content:"",
-            dificulty:"",
-            requirement:"",
-            learnTo:[],
-            studyMethod:""
-        })
+        try {
+            const response = await axios.post(
+              `http://localhost:3001/courses/`,
+              {
+                title,
+                description,
+                image: productImg,
+                lenguage,
+                price,
+                pro,
+                duration, 
+                content,
+                dificulty,
+                requirement,
+                learnTo: learnTo.split(','),
+                studyMethod
+              }  
+            );
+            setPreviewSource('')
+            setProductImg('');
+            setTitle('')
+            setDescription('')
+            setLenguage('')
+            setPrice('')
+            setPro('')
+            setDuration('')
+            setContent('')
+            setDificulty('')
+            setRequirement('')
+            setCategory('')
+            setLearnTo('')
+      
+            return response.data;
+          } catch (error) {
+            console.log(error);
+          }
     }
 
-    const handleInput = (event) => {
-        const { name, value } = event.target;
-
-        setCourse({
-            ...course,
-            [name]: value
-        });
-    }
-
-    const handleFileInputChange = (e) => {
+      const handleProductImageUpload = (e) => {
         const file = e.target.files[0];
-        previewFile(file);
-        setSelectedFile(file);
-        setFileInputState(e.target.value);
-    };
+        TransformFileData(file);
+        previewFile(file)
+        
+      };
 
-    const previewFile = (file) => {
+      const handlePromo = () => {
+        if(pro === true) setPro(false);
+        if(pro === false) setPro(true);
+      }
+
+      const previewFile = (file) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = () => {
             setPreviewSource(reader.result);
         };
     };
-
-    const uploadImage = async (base64EncodedImage) => {
-        try {
-            // await fetch('/api/upload', {
-            //     method: 'POST',
-            //     body: JSON.stringify({ data: base64EncodedImage }),
-            //     headers: { 'Content-Type': 'application/json' },
-            // });
-            setFileInputState('');
-            setPreviewSource('');
-
-        } catch (err) {
-            console.error(err);
-          
+    
+    const TransformFileData = (file) => {
+        const reader = new FileReader();
+    
+        if (file) {
+          reader.readAsDataURL(file);
+          reader.onloadend = () => {
+            setProductImg(reader.result);
+          };
+        } else {
+          setProductImg("");
         }
-    };
+      };
+    
 
   return (
     <form 
     className={styles.form}
     onSubmit={handleSubmit}
     >
-        <input
-            id="fileInput"
-            type="file"
-            name="image"
-            onChange={handleFileInputChange}
-            value={fileInputState}
-            className="form-input"
-        />
+        <label className={styles.inputImageLabel}> 
+            Seleccionar Imagen
+            <input
+                id="fileInput"
+                type="file"
+                accept="image/*"
+                onChange={handleProductImageUpload}
+                className={styles.inputImage}
+                required
+            />
+        </label>
+       
         {previewSource && (
             <img
                 src={previewSource}
                 alt="chosen"
-                style={{ height: '300px' }}
+                style={{ 
+                  height: '300px', MaxWidth: '700px' 
+                }}
             />
         )}
-        <input 
-            type="text" 
-            placeholder="title"
-            name="title"
-            value={course.title}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        <input 
-            type="text" 
-            placeholder="description"
-            name="description"
-            value={course.description}
-            onChange={handleInput}
-            className={styles.input}
-        />
-         <input 
-            type="text" 
-            placeholder="lenguage"
-            name="lenguage"
-            value={course.lenguage}
-            onChange={handleInput}
-            className={styles.input}
-        />
-         <input 
-            type="number" 
-            placeholder="price"
-            name="price"
-            value={course.price}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        {/* Pro */}
-        <input 
-            type="text" 
-            placeholder="duration"
-            name="duration"
-            value={course.duration}
-            onChange={handleInput}
-            className={styles.input}
-        />
-          <input 
-            type="text" 
-            placeholder="Content"
-            name="content"
-            value={course.content}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        <input 
-            type="text" 
-            placeholder="Difficulty"
-            name="dificulty"
-            value={course.dificulty}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        <input 
-            type="text" 
-            placeholder="requirement"
-            name="requirement"
-            value={course.requirement}
-            onChange={handleInput}
-            className={styles.input}
-        />
-         <input 
-            type="text" 
-            placeholder="learnTo"
-            name="learnTo"
-            value={course.learnTo}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        <input 
-            type="text" 
-            placeholder="studyMethod"
-            name="studyMethod"
-            value={course.studyMethod}
-            onChange={handleInput}
-            className={styles.input}
-        />
-        {/* catego */}
+
+        <div className={styles.container}>
+            <input 
+                type="text" 
+                placeholder="Título de tu publicación"
+                onChange={(e) => setTitle(e.target.value)}
+                className={styles.input}
+            />
+            <input 
+                type="text" 
+                placeholder="Describe tu publicación"
+                onChange={(e) => setDescription(e.target.value)}
+                className={styles.input}
+            />
+            <textarea
+                type="text" 
+                placeholder="Contenido de tu publicación"
+                onChange={(e) => setContent(e.target.value)}
+                className={styles.textarea}
+            />
+            <textarea  
+                placeholder="Método de estudio"
+                onChange={(e) => setStudyMethod(e.target.value)}
+                className={styles.textarea}
+            />
+            <textarea  
+                placeholder="Método de Entrega"
+                className={styles.textarea}
+            />
+            <textarea  
+                placeholder="Realización de entrega"
+                className={styles.textarea}
+            />
+        </div>
+        <div className={styles.container}>      
+            <select
+                onChange={(e) => setDificulty(e.target.value)}
+                className={styles.inputSelect}
+            >
+                <option disabled="" value="">
+                Elige un dificultad
+                </option>
+                <option value="BASIC">Principiante</option>
+                <option value="MEDIUM">Intermedio</option>
+                <option value="ADVANCED">Avanzado</option>
+            </select>
+            <select
+                onChange={(e) => setCategory(e.target.value)}
+                className={styles.inputSelect}
+            >
+                <option disabled="" value="">
+                Elige una categoria
+                </option>
+                {categoryOptions.map(({value, name}) => (
+                    <option value={value} key={value}>
+                    {name}
+                    </option>
+                ))}
+            </select>
+            <input 
+                type="number" 
+                placeholder="price"
+                onChange={(e) => setPrice(e.target.value)}
+                className={styles.input}
+            />  
+            <select
+                onChange={(e) => setLenguage(e.target.value)}
+                className={styles.inputSelect}
+            >
+                <option disabled="" value="">
+                    Elige un idioma
+                </option>
+                <option value="ESPAÑOL">Español</option>
+                <option value="ENGLISH">Inglés</option>
+            </select>   
+            <div className={styles.checkboxContainer}>
+                <input 
+                    type="checkbox" 
+                    onChange={handlePromo}
+                /> 
+                <label className={styles.label} >Aplicar promoción de 30% de descuento</label>  
+            </div>
+         
+            <input 
+                type="text" 
+                placeholder="Duración estimada"
+                onChange={(e) => setDuration(e.target.value)}
+                className={styles.input}
+            /> 
+            <textarea 
+                placeholder="requirement"
+                onChange={(e) => setRequirement(e.target.value)}
+                className={styles.textarea}
+            />
+            <input 
+                type="text" 
+                placeholder="learnTo"
+                onChange={(e) => setLearnTo(e.target.value)}
+                className={styles.input}
+            />
+        </div>       
+       
         <button className={styles.button} type="submit">
             <span className={styles.button_text}>Crea tu publicación</span>
         </button>
