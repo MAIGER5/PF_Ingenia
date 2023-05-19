d5fa9bd54f82c33d2f15e6c63e928c5799cb90f6
Davidc480 <Davidcalle.480@gmail.com>
2023-05-19 14:15:46 -0500

    refactor STUDEN a STUDENT

--- api/src/controllers/rellenarControllers/rellenarControllers.js
+++ api/src/controllers/rellenarControllers/rellenarControllers.js
@@ -1,54 +1,54 @@
-const {User,Category,Course} = require('../../database')
-const createCourse = require('../courseControllers/createCourse')
+const { User, Category, Course } = require("../../database");
+const createCourse = require("../courseControllers/createCourse");
 
+const categorias = require("./categorias");
+const instructores = require("./instructores");
+const cursos = require("./cursos");
 
-const categorias = require('./categorias')
-const instructores = require('./instructores')
-const cursos = require('./cursos')
+const rellenarControllers = async () => {
+  for (let i = 0; i < categorias.length; i++) {
+    const name = categorias[i].name;
+    const newCategory = await Category.create({ name });
+  }
+  for (let i = 0; i < instructores.length; i++) {
+    const obj = instructores[i];
+    const newInstr = await User.create(obj);
+  }
+  for (let i = 0; i < cursos.length; i++) {
+    const {
+      title,
+      description,
+      image,
+      lenguage,
+      price,
+      pro,
+      duration,
+      content,
+      dificulty,
+      requirement,
+      learnTo,
+      studyMethod,
+      users,
+      catego,
+    } = cursos[i];
+    const newCurso = await createCourse(
+      title,
+      description,
+      image,
+      lenguage,
+      price,
+      pro,
+      duration,
+      content,
+      dificulty,
+      requirement,
+      learnTo,
+      studyMethod,
+      users,
+      catego
+    );
+  }
 
-const rellenarControllers = async ()=>{
-    for(let i = 0;i<categorias.length;i++){
-        const name = categorias[i].name
-        const newCategory = await Category.create({name})
-    }
-    for(let i = 0;i<instructores.length;i++){
-        const obj = instructores[i]
-        const newInstr = await User.create(obj)
-    }
-    for(let i = 0;i<cursos.length;i++){
-        const {
-            title,
-            description,
-            image,
-            lenguage,
-            price,
-            pro,
-            duration,
-            content,
-            dificulty,
-            requirement,
-            learnTo,
-            studyMethod,
-            users,
-            catego} = cursos[i]
-            const newCurso = await createCourse(
-                title,
-                description,
-                image,
-                lenguage,
-                price,
-                pro,
-                duration,
-                content,
-                dificulty,
-                requirement,
-                learnTo,
-                studyMethod,
-                users,
-                catego
-            )
-    }
-
-    return {Data:'Agregada'}
-}
+  return { Data: "Agregada" };
+};
 module.exports = rellenarControllers;
