d83cbe7539def4df096d9d61ca95874dc31e9a36
MauricioTejada <mauriciotejada14@gmail.com>
2023-05-20 00:05:21 -0300

    Mauricio

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeComponent from '../Components/Home/Homevista/Home'
+import NotificationSnackbar from '../Components/NotificationSnackbar/NotificationSnackbar'
 
 
 export default function Home() {
   return (
     <div>
+      {/* <NotificationSnackbar/> */}
       <HomeComponent/>
     </div>
   )
