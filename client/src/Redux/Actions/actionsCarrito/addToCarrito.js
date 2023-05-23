94b38cece26156f22f73ab02ea8715733e1113ae
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-05-23 12:09:14 -0500

    configurando la action addcarrito con la ruta

--- client/src/Redux/Actions/actionsCarrito/addToCarrito.js
+++ client/src/Redux/Actions/actionsCarrito/addToCarrito.js
@@ -3,10 +3,12 @@ import axios from "axios";
 export const ADD_TO_CARRITO = 'ADD_TO_CARRITO';
 
 const URLcourse = 'http://localhost:3001/courses'
+const URLaddCarBd = 'http://localhost:3001/buy/carrito?idCourse=1&idUser=1'
 
-export const addToCarrito = (id) => {
+export const addToCarrito = (idCourse) => {
   return async function(dispatch) {
-    const response = await axios.get(`${URLcourse}/${id}`);
+    const response = await axios.get(`${URLcourse}/${idCourse}`);
+    const response2 = await axios.post(`${URLaddCarBd}?idCourse=${idCourse}&idUser=${idUser}`)
     dispatch({type: ADD_TO_CARRITO, payload: response.data});
 };
   
