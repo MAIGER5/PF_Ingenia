07cb32c994ba932454759d7832ca7ad5d50f370f
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-05-23 14:01:52 -0500

    action carrito

--- client/src/Redux/Actions/actionsCarrito/addToCarrito.js
+++ client/src/Redux/Actions/actionsCarrito/addToCarrito.js
@@ -3,12 +3,10 @@ import axios from "axios";
 export const ADD_TO_CARRITO = 'ADD_TO_CARRITO';
 
 const URLcourse = 'http://localhost:3001/courses'
-const URLaddCarBd = 'http://localhost:3001/buy/carrito?idCourse=1&idUser=1'
 
-export const addToCarrito = (idCourse) => {
+export const addToCarrito = (id) => {
   return async function(dispatch) {
-    const response = await axios.get(`${URLcourse}/${idCourse}`);
-    const response2 = await axios.post(`${URLaddCarBd}?idCourse=${idCourse}&idUser=${idUser}`)
+    const response = await axios.get(`${URLcourse}/${id}`);
     dispatch({type: ADD_TO_CARRITO, payload: response.data});
 };
   
