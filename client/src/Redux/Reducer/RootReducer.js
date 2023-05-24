5494b91f99ebc2ec3cdad0db7ade2e20d5850416
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-05-23 20:03:29 -0500

    corrigiendo reducer

--- client/src/Redux/Reducer/RootReducer.js
+++ client/src/Redux/Reducer/RootReducer.js
@@ -11,7 +11,7 @@ import { FILTER_BY_PRICE } from "../Actions/filterByPrice";
 import { ADD_TO_CARRITO } from "../Actions/actionsCarrito/addToCarrito";
 import { POST_LOCAL_STORAGE} from "../Actions/actionsCarrito/postLocalStorage";
 import { REMOVE_ONE_FROM_CARRITO } from "../Actions/actionsCarrito/RemoveOneFromCarrito";
-// import { GET_TO_CARRITO_BD } from "../Actions/getToCarritoBd";
+import { GET_TO_CARRITO_BD } from "../Actions/getToCarritoBd";
 
 const initialState = {
     allCourse: [],
@@ -94,13 +94,13 @@ const rootReducer = (state = initialState, action) => {
                 allCarrito: [...state.allCarrito, action.payload],
 
             };
-        // case GET_TO_CARRITO_BD :
+        case GET_TO_CARRITO_BD :
 
-        //     return {
-        //         ...state,
-        //         allCarrito: [...state.allCarrito, action.payload],
+            return {
+                ...state,
+                allCarrito: [...state.allCarrito, action.payload],
 
-        //     };
+            };
         case REMOVE_ONE_FROM_CARRITO:
 
             return {
