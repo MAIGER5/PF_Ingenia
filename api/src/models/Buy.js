3723b6410b8555f942fd2183d91ecf7286e728a3
new_proyect_01 <mafilz145@gmail.com>
2023-05-24 19:10:31 -0400

    cambio en el modelo buy

--- api/src/models/Buy.js
+++ api/src/models/Buy.js
@@ -10,34 +10,6 @@ module.exports = (sequelize) => {
     idCor:{
       type: DataTypes.INTEGER,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      isUrl:true,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    pro:{
-      type:DataTypes.BOOLEAN,
-      default:false
-    },
-    pricePro:{
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    userInstrucotor:{
-      type:DataTypes.STRING
-    }
   },{
     timestamps:false,
 });
