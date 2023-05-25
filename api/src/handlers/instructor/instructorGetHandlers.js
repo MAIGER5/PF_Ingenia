53912629055692f3955bee968714a556161e1b67
fellgar246 <fellgar246@gmail.com>
2023-05-25 13:40:13 -0600

    refactor(Client:FormCourses,FormArticle,Profile)

--- api/src/handlers/instructor/instructorGetHandlers.js
+++ api/src/handlers/instructor/instructorGetHandlers.js
@@ -1,6 +1,7 @@
 const createInstructor = require("../../controllers/instructorControllers/createInstructor");
 const verifyUserExistence = require("../../helper/verifyUserExistence");
 const bcryptjs = require("bcryptjs");
+const { cloudinary } = require('../../utils/cloudinary');
 const {
   publicationControllers,
   publisById,
@@ -32,11 +33,48 @@ const instructorPostHandlers = async (req, res) => {
     res.status(400).json({ error: error.message, createVerification: false });
   }
 };
+
 const publicationsPostHandlers = async (req,res)=>{
   const {idUser,title,subtitle,text,img,subtitleTwo,textTwo,subtitleThree,textThree} = req.body
+  
   try {
-    const response = await publicationControllers(idUser,title,subtitle,text,img,subtitleTwo,textTwo,subtitleThree,textThree)
-    res.status(200).json(response);
+
+    if(img.length > 150){
+      const uploadImage = await cloudinary.uploader.upload(img, {
+        upload_preset: 'ingenia',
+      });
+
+      if(uploadImage){
+        const response = await publicationControllers(
+        idUser,
+        title,
+        subtitle,
+        text,
+        uploadImage.url,
+        subtitleTwo,
+        textTwo,
+        subtitleThree,
+        textThree
+        )
+        res.status(200).json(response);
+      }
+
+    } else {
+
+      const response = await publicationControllers(
+        idUser,
+        title,
+        subtitle,
+        text,
+        img,
+        subtitleTwo,
+        textTwo,
+        subtitleThree,
+        textThree
+      )
+      res.status(200).json(response);
+
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
---
+++ client/src/Components/FormArticle/validations.js
@@ -0,0 +1,24 @@
+export function validationArticle(inputs) {
+  const errors = {}
+
+  if (!inputs.title) errors.title = 'El título es obligatorio';
+  if (inputs.title.length > 80) errors.title = 'Máximo de 80 caracteres';
+
+  if (!inputs.subtitle) errors.subtitle = 'El subtítulo es obligatorio';
+  if (inputs.subtitle.length > 80) errors.subtitle = 'Máximo de 80 caracteres';
+
+  if (!inputs.text) errors.text = 'El primer párrafo es obligatorio';
+  if (inputs.text.length > 1000) errors.text = 'Máximo de 1000 caracteres';
+
+  if (inputs.subtitleTwo.length > 80) errors.subtitleTwo = 'Máximo de 80 caracteres';
+  if(inputs.subtitleTwo && !inputs.textTwo) errors.textTwo = "Es requerido ingreses el texto del segundo párrafo"
+  if (inputs.textTwo.length > 1000) errors.textTwo = 'Máximo de 1000 caracteres';
+
+  if (inputs.subtitleThree.length > 80) errors.subtitleThree = 'Máximo de 80 caracteres';
+  if(inputs.subtitleThree && !inputs.textThree) errors.textThree = "Es requerido ingreses el texto del segundo párrafo"
+  if (inputs.textThree.length > 1000) errors.textThree = 'Máximo de 1000 caracteres';
+
+
+
+  return errors;
+}
