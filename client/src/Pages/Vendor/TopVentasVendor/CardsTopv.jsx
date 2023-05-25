2b35a58d6ce1cf658808d89d6a283968c9ea4332
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-05-24 21:55:04 -0500

    creando la vista perfil vendedor publico

---
+++ client/src/Pages/Vendor/TopVentasVendor/CardsTopv.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Box, Grid } from '@mui/material'
+import { useSelector } from 'react-redux';
+import CardHome from '../../../Components/Card/CardHome';
+
+
+export const CardsTopv = () => {
+
+    const curs = useSelector((state)=> state.allCourse)
+    const cursos = curs.slice(0, 3);
+
+    return (
+        <Box  >
+            <Grid container spacing={10}>
+                {cursos.map((curso) => (
+                    <Grid item xs={2} sm={6} md={4} key={curso.idCourse}>
+                        <CardHome
+                            idCourse = {curso.idCourse}
+                            title={curso.title}
+                            lenguage={curso.lenguage}
+                            image={curso.image}
+                            instructorLastName={curso.Users[0]?.lastname}
+                            instructorName={curso.Users[0]?.name}
+                            price={curso.price}
+                            habilities={curso.habilities}
+                    />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    )
+}
--- client/src/Pages/Vendor/VendedorPublico.jsx
+++ client/src/Pages/Vendor/VendedorPublico.jsx
@@ -1,16 +1,78 @@
-import { Box, Grid, Tab, Tabs } from "@mui/material"
+import { Box, Grid, Tab, Tabs, Typography } from "@mui/material"
 import { CardVendedor } from "./CardVendedor"
+import { TopVentasVendor } from "./TopVentasVendor/TopVentasVendor"
+import { PublicacionesRecientes } from "./Publicaciones/PublicacionesRecientes"
+import { useState } from "react"
+import VendorArtículos from "../../Components/VendorViews/VendorArticulos/VendorArtículos"
+import VendorValoraciones from "../../Components/VendorViews/VendorValoraciones/VendorValoraciones"
+import VendorCursos from "../../Components/VendorViews/VendorCursos/VendorCursos"
 
 
 export const VendedorPublico = () => {
+
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+      setTabIndex(newTabIndex);
+  };
+  
   return (
-    <div>
+    <Grid>
         <CardVendedor/>
 
-        <Grid>
-            <Grid>Mas vendidos</Grid>
-            <Grid>Publicación Reciente</Grid>
+        <Grid container spacing={2}>
+
+            <Grid item xs={6.5} marginLeft={20} marginTop={10}>
+              <TopVentasVendor/>
+            </Grid>
+
+            <Grid item xs={0} marginTop={10}>
+              <PublicacionesRecientes/>
+            </Grid>
+            
         </Grid>
-    </div>
+        <div>
+          <Box>
+              <Tabs 
+                value={tabIndex} 
+                onChange={handleTabChange}
+                centered
+              >
+              <Tab 
+                label="Cursos" 
+                sx={{ fontSize: "16px" }}
+              />
+              <Tab 
+                label="Artículos" 
+                sx={{ fontSize: "16px" }}  
+              />
+              <Tab 
+                label="Valoraciones" 
+                sx={{ fontSize: "16px" }}  
+              />
+              </Tabs>
+              <Box sx={{ padding: 2 }}>
+              {tabIndex === 0 && (
+                <Box>
+                  <p>Cursos</p>
+                  <VendorCursos />
+                </Box>
+              )}
+              {tabIndex === 1 && (
+                <Box>
+                  <p>Articulos</p>
+                  <VendorArtículos />
+                </Box>
+              )}
+               {tabIndex === 2 && (
+                <Box>
+                    <p>Valoraciones</p>
+                    <VendorValoraciones />
+                </Box>
+              )}
+              </Box>
+          </Box>
+        </div>
+    </Grid>
   )
 }
