8e775343eb2ff7d591e09525517a32ec04d72f82
Davidc480 <Davidcalle.480@gmail.com>
2023-05-26 10:07:12 -0500

    feat(controller - handler/user/updateInfo) ruta de updated info

--- api/src/handlers/user/updateInfo/updateInfoHandler.js
+++ api/src/handlers/user/updateInfo/updateInfoHandler.js
@@ -1,16 +1,24 @@
+const updateInfoController = require("../../../controllers/userControllers/updatedInfo/updateInfoUserController");
+
 const updateInfoHandler = async (req, res) => {
-  const { name, lastaname, password, userType } = req.body;
+  const { idUser, name, lastaname, password } = req.body;
 
   try {
     const userInfo = await updateInfoController(
       name,
       lastaname,
       password,
-      userType
+      idUser
     );
-    res.status(400).json(userInfo);
+    if (userInfo[0] === 1) {
+      res.status(200).json({ process: true });
+    } else {
+      throw new Error(
+        "En este momento no podemos procesar tu solicitud, intentalo mas tardé"
+      );
+    }
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message, process: false });
   }
 };
 
