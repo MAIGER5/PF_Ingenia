152f03dc497321084c20b17abfebdb36afd169ce
Davidc480 <Davidcalle.480@gmail.com>
2023-05-26 17:17:19 -0500

    refactor(api/src/controllers/instructorControllers/userDeletecontroller and handler) modificaciones para que sea mas sencillo para el front, solo es necesario el id del usuario para habilitar y deshabilitar

--- api/src/controllers/userControllers/userDeleteController.js
+++ api/src/controllers/userControllers/userDeleteController.js
@@ -1,27 +1,32 @@
 const { User } = require("../../database");
 
-const userDeleteController = async (idUser, asset) => {
-  if (asset) {
-    const deleteUser = await User.update(
-      { asset: false },
-      {
-        where: {
-          idUser,
-        },
-      }
-    );
-    return { deleteUser };
-  } else {
-    const deleteUser = await User.update(
-      { asset: true },
-      {
-        where: {
-          idUser,
-        },
-      }
-    );
-    return { deleteUser };
+const userDeleteController = async (idUser) => {
+  const user = await User.findByPk(idUser);
+
+  if (user) {
+    if (user.asset) {
+      const userState = await User.update(
+        { asset: false },
+        {
+          where: {
+            idUser,
+          },
+        }
+      );
+      return { userState, state: "Deshabilitado" };
+    } else {
+      const userState = await User.update(
+        { asset: true },
+        {
+          where: {
+            idUser,
+          },
+        }
+      );
+      return { userState, state: "Habilitado" };
+    }
   }
+  throw new Error("El usuario no se encuentra registrado");
 };
 
 module.exports = userDeleteController;
