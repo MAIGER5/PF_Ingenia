1bd08494bf6871d822ffcc61a51eb81d98323fcc
Davidc480 <Davidcalle.480@gmail.com>
2023-05-26 13:10:35 -0500

    feat(controller - handler/courseControllers/courseDeleteController) ruta de borrado logico usuario y cursos

--- api/src/controllers/userControllers/userDeleteController.js
+++ api/src/controllers/userControllers/userDeleteController.js
@@ -1,16 +1,27 @@
 const { User } = require("../../database");
 
-const userDeleteController = async (email, userType) => {
-  const deleteUser = await User.update(
-    { asset: false },
-    {
-      where: {
-        email,
-        Is: userType,
-      },
-    }
-  );
-  return { user: deleteUser };
+const userDeleteController = async (idUser, asset) => {
+  if (asset) {
+    const deleteUser = await User.update(
+      { asset: false },
+      {
+        where: {
+          idUser,
+        },
+      }
+    );
+    return { deleteUser };
+  } else {
+    const deleteUser = await User.update(
+      { asset: true },
+      {
+        where: {
+          idUser,
+        },
+      }
+    );
+    return { deleteUser };
+  }
 };
 
 module.exports = userDeleteController;
