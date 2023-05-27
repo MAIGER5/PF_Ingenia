282869e2e935d0023a320887326ea975089d39fc
Davidc480 <Davidcalle.480@gmail.com>
2023-05-26 22:23:18 -0500

    ruta borrado logico cursos

--- api/src/controllers/courseControllers/courseDeleteController.js
+++ api/src/controllers/courseControllers/courseDeleteController.js
@@ -1,31 +1,26 @@
-const { Course, User, sequelize } = require("../../database");
+const { Course } = require("../../database");
 
 const courseDeleteController = async (idCourse) => {
   const course = await Course.findByPk(idCourse);
 
   if (course) {
-    // Obtener los usuarios asociados al curso
-    const users = await course.getUsers();
-
-    if (users && users.length > 0) {
-      // Actualizar la propiedad 'asset' para cada relación UserCourse
-      const promises = users.map((user) =>
-        user.UserCourse.update(
-          { asset: false },
-          { where: { CourseIdCourse: idCourse } }
-        )
+    if (course.asset) {
+      const courseState = await Course.update(
+        { asset: false },
+        { where: { idCourse } }
       );
-
-      // Ejecutar todas las actualizaciones en paralelo
-      await Promise.all(promises);
-
-      return true;
+      return courseState;
     } else {
-      throw new Error("El curso no está asociado a ningún usuario");
+      const courseState = await Course.update(
+        { asset: true },
+        { where: { idCourse } }
+      );
+      return courseState;
     }
   }
-
-  throw new Error("No se encontró el curso especificado");
+  throw new Error(
+    "Lo sentimos no a sido posible encontrar el curso especificado"
+  );
 };
 
 module.exports = courseDeleteController;
