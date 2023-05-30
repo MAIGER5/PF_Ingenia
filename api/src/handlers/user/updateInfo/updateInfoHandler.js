ce5c1cf655d9abc363fbf7f5af8b10cd64931a73
Davidc480 <Davidcalle.480@gmail.com>
2023-05-29 23:05:48 -0500

    refactor(api/src/handlers/user/updateInfo/updateInfoHandler) ruta de updated completa

--- api/src/handlers/user/updateInfo/updateInfoHandler.js
+++ api/src/handlers/user/updateInfo/updateInfoHandler.js
@@ -1,35 +1,38 @@
 const updateInfoController = require("../../../controllers/userControllers/updatedInfo/updateInfoUserController");
-const { cloudinary } = require('../../../utils/cloudinary.js');
+const { cloudinary } = require("../../../utils/cloudinary.js");
 const bcrypt = require("bcryptjs");
 
 const updateInfoHandler = async (req, res) => {
   const { idUser, name, lastname, imgProfile, description, studies, password } =
     req.body;
-
-  const passwordHash = await bcrypt.hash(password, 8);
-
   try {
+    if (password && imgProfile) {
+      // Actualizar tanto el password como la imagen de perfil
+      const passwordHash = await bcrypt.hash(password, 8);
+      if (imgProfile.length > 150) {
+        const uploadImage = await cloudinary.uploader.upload(imgProfile, {
+          upload_preset: "ingenia",
+        });
 
-    if(imgProfile.length > 150){
-      const uploadImage = await cloudinary.uploader.upload(imgProfile, {
-        upload_preset: 'ingenia',
-      });
-
-      if(uploadImage){
-        const userInfo = await updateInfoController(
-          idUser,
-          name,
-          lastname,
-          uploadImage,
-          description,
-          studies,
-          passwordHash
-        );
+        if (uploadImage) {
+          const userInfo = await updateInfoController(
+            idUser,
+            name,
+            lastname,
+            uploadImage,
+            description,
+            studies,
+            passwordHash
+          );
+          const { state } = userInfo;
+          userInfo.updateInfo[0]
+            ? res.status(200).json({ state, process: true })
+            : null;
+        }
       }
-
-
-    } else {
-
+    } else if (password) {
+      // Actualizar solo el password
+      const passwordHash = await bcrypt.hash(password, 8);
       const userInfo = await updateInfoController(
         idUser,
         name,
@@ -39,17 +42,48 @@ const updateInfoHandler = async (req, res) => {
         studies,
         passwordHash
       );
-    
+      const { state } = userInfo;
+      userInfo.updateInfo[0]
+        ? res.status(200).json({ state, process: true })
+        : null;
+    } else if (imgProfile) {
+      // Actualizar solo la imagen de perfil
+      if (imgProfile.length > 150) {
+        const uploadImage = await cloudinary.uploader.upload(imgProfile, {
+          upload_preset: "ingenia",
+        });
+
+        if (uploadImage) {
+          const userInfo = await updateInfoController(
+            idUser,
+            name,
+            lastname,
+            uploadImage,
+            description,
+            studies,
+            password
+          );
+          const { state } = userInfo;
+          userInfo.updateInfo[0]
+            ? res.status(200).json({ state, process: true })
+            : null;
+        }
+      }
+    } else {
+      const userInfo = await updateInfoController(
+        idUser,
+        name,
+        lastname,
+        imgProfile,
+        description,
+        studies
+      );
+      const { state } = userInfo;
+
+      userInfo.updateInfo[0]
+        ? res.status(200).json({ state, process: true })
+        : null;
     }
-
-
-   
-
-    const { state } = userInfo;
-
-    userInfo.updateInfo[0]
-      ? res.status(200).json({ state, process: true })
-      : null;
   } catch (error) {
     res.status(400).json({ error: error.message, process: false });
   }
