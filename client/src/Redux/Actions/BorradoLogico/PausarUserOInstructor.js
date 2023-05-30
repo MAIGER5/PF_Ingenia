d956f5af986ca956bd7b7970f3c94bc5fed68314
fellgar246 <fellgar246@gmail.com>
2023-05-29 22:13:02 -0600

    refactor(Redux/BorradoLógico)

--- client/src/Redux/Actions/BorradoLogico/PausarUserOInstructor.js
+++ client/src/Redux/Actions/BorradoLogico/PausarUserOInstructor.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 export const PAUSAR_USER_INSTRUCTOR = 'PAUSAR_USER_INSTRUCTOR';
 
-const URLpausarUserInstructor = 'http://localhost:3001/user/delete'
-
 
 export const PausarUserOInstructor = (idUser) => {
     return async function(){
-        await axios.put(`${URLpausarUserInstructor}`, {"idUser":idUser});
+        await axios.put(`${import.meta.env.VITE_HOST}/user/delete`, {"idUser":idUser});
       }
 }
