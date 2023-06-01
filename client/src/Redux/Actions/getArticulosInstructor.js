867ea6a89af234cfaf3281ad7eaa790387e985c4
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2023-06-01 00:29:14 -0500

    modificando actions de detalle del instructor

--- client/src/Redux/Actions/getArticulosInstructor.js
+++ client/src/Redux/Actions/getArticulosInstructor.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 export const GET_ARTICULOS_INSTRUCTOR = 'GET_ARTICULOS_INSTRUCTOR';
 
-export const getArticulosInstructor = (nameInstructor) => {
+export const getArticulosInstructor = (idInstructor) => {
     return async function(dispatch) {
         const articuls = await axios.get(`${import.meta.env.VITE_HOST}/instructor/publication`);
-        const response = articuls.data?.filter((art)=>art.User?.name === nameInstructor)
+        const response = articuls.data?.filter((art)=>art.User?.UserIdUser === idInstructor)
         dispatch({type: GET_ARTICULOS_INSTRUCTOR, payload: response})
       }
 }
--- client/src/Redux/Actions/getCoursesInstructor.js
+++ client/src/Redux/Actions/getCoursesInstructor.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export const GET_COURSES_INSTRUCTOR = 'GET_COURSES_INSTRUCTOR';
 
 
-export const getCoursesInstructor = (nameInstructor) => {
+export const getCoursesInstructor = (idInstructor) => {
     return async function(dispatch) {
         const courses = await axios.get(`${import.meta.env.VITE_HOST}/courses/`);
-        const response = courses.data?.filter((cur)=>cur.Users[0]?.name == nameInstructor)
+        const response = courses.data?.filter((cur)=>cur.Users[0]?.idUser == idInstructor)
         dispatch({type: GET_COURSES_INSTRUCTOR, payload: response})
       }
 }
