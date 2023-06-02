25977d857b5ee65674b33a53e6ca5653210e4d84
MauricioTejada <mauriciotejada14@gmail.com>
2023-06-02 16:17:20 -0300

    Configurada la variable de entorno para buscar info al servidor local

--- client/env.js
+++ client/env.js
@@ -1,6 +1,11 @@
+// const env = {
+//   VITE_FB_API_KEY: "AIzaSyD3ToUSR1jwFA5oUswh7AlCij7HZRjzyk0",
+//   VITE_HOST: "https://pfingenia-production.up.railway.app",
+// };
+
 const env = {
   VITE_FB_API_KEY: "AIzaSyD3ToUSR1jwFA5oUswh7AlCij7HZRjzyk0",
-  VITE_HOST: "https://pfingenia-production.up.railway.app",
+  VITE_HOST: "https://http://localhost:3001",
 };
 
 export default env;
